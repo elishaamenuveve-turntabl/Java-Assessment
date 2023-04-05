14c787a897befc0195e2fbcd8750ab8e626a7ffb
elisha.amenuveve <elisha.amenuveve@turntabl.io>
2023-04-05 16:28:57 +0000

    implemented create total builds log

--- src/main/java/com/vmorg/buildrequest/MachineRequestor.java
+++ src/main/java/com/vmorg/buildrequest/MachineRequestor.java
@@ -1,18 +1,35 @@
 package com.vmorg.buildrequest;
 
 import com.vmorg.auth.AuthorisingService;
+import com.vmorg.build.SystemBuildService;
 import com.vmorg.exception.MachineNotCreatedException;
 import com.vmorg.exception.UserNotEntitledException;
 import com.vmorg.machine.Machine;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class MachineRequestor implements VirtualMachineRequestor{
+    private static Map<String, Map<String, Integer>> totalBuildsByUserForToday;
+    private Map<String, Integer> totalBuildsToday;
+    private static int totalFailedBuildsForToday;
+
+
+    private static List<Machine>  collectionOfMachinesBuiltToday = new ArrayList<>();
+    private static List<Machine>  collectionOfFailedBuildsToday = new ArrayList<>();
+
+//    private static List<VirtualMachineRequestor>  failedRequests = new ArrayList<>();
     private Machine machine;
     private final String username;
     private AuthorisingService authorisingService;
-    public MachineRequestor(String username, AuthorisingService authorisingService) {
+
+    private SystemBuildService systemBuildService;
+
+    public MachineRequestor(String username, AuthorisingService authorisingService, SystemBuildService systemBuildService) {
         this.authorisingService = authorisingService;
+        this.systemBuildService = systemBuildService;
         this.username = username;
     }
 
@@ -31,7 +48,17 @@ public class MachineRequestor implements VirtualMachineRequestor{
     @Override
     public void createNewRequest(Machine machine) throws UserNotEntitledException, MachineNotCreatedException {
         if (authorisingService.isAuthorised(username)) {
-            //
+           String hostname = systemBuildService.createNewMachine(machine);
+           if (Objects.equals(hostname, "")) {
+               MachineRequestor.totalFailedBuildsForToday ++;
+               throw new MachineNotCreatedException();
+           } else {
+               totalBuildsToday.putIfAbsent(machine.getClass().getSimpleName(), 0);
+               totalBuildsToday.put(machine.getClass().getSimpleName(), 1);
+               totalBuildsByUserForToday.put(username, totalBuildsToday);
+               collectionOfMachinesBuiltToday.add(machine);
+               MachineRequestor.collectionOfMachinesBuiltToday.add(machine);
+           }
         } else {
             throw new UserNotEntitledException();
         }
@@ -44,8 +71,9 @@ public class MachineRequestor implements VirtualMachineRequestor{
         return null;
     }
 
+
     @Override
     public int totalFailedBuildsForDay() {
-        return 0;
+        return totalFailedBuildsForToday;
     }
 }
--- src/test/java/com/vmorg/buildrequest/BuildRequestTest.java
+++ src/test/java/com/vmorg/buildrequest/BuildRequestTest.java
@@ -2,6 +2,7 @@ package com.vmorg.buildrequest;
 
 
 import com.vmorg.auth.AuthorisingService;
+import com.vmorg.build.SystemBuildService;
 import com.vmorg.exception.MachineNotCreatedException;
 import com.vmorg.exception.UserNotEntitledException;
 import com.vmorg.machine.Desktop;
@@ -13,35 +14,76 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BuildRequestTest {
+
+    String username = "Noah";
     VirtualMachineRequestor requestor;
     @Mock
     Machine newMachine;
 
     @Mock
     AuthorisingService authorisingService;
+    @Mock
+    SystemBuildService systemBuildService;
 
     @BeforeEach
     void setUp() {
         authorisingService = mock(AuthorisingService.class);
-        requestor = new MachineRequestor("Noah", authorisingService);
+        requestor = new MachineRequestor("Noah", authorisingService, systemBuildService);
+        when(authorisingService.isAuthorised(username)).thenReturn(true);
 
     }
 
     @Test
-    void vmCreatedUserNotEntitled() throws MachineNotCreatedException, UserNotEntitledException {
-        String username = "Noah";
+    void requestorThrowsExceptionWhenUserNotEntitled() throws MachineNotCreatedException, UserNotEntitledException {
         when(authorisingService.isAuthorised(username)).thenReturn(false);
-
-
         Assertions.assertThrows(UserNotEntitledException.class, ()->requestor.createNewRequest(newMachine));
+    }
 
+    @Test
+    void requestorDoesNotThrowsExceptionWhenUserIsEntitled() throws MachineNotCreatedException, UserNotEntitledException {
+        when(authorisingService.isAuthorised(username)).thenReturn(true);
+        when(systemBuildService.createNewMachine(newMachine)).thenReturn("host20230328005");
+        Assertions.assertDoesNotThrow(()->requestor.createNewRequest(newMachine));
 
     }
+
+    @Test
+    void requestorCreatesRequestWhenUserIsEntitledTest() throws MachineNotCreatedException, UserNotEntitledException {
+        when(systemBuildService.createNewMachine(newMachine)).thenReturn("host20230328005");
+        String username = "Noah";
+        when(authorisingService.isAuthorised(username)).thenReturn(true);
+        requestor.createNewRequest(newMachine);
+        verify(systemBuildService,times(1)).createNewMachine(newMachine);
+    }
+
+    @Test
+    void requestorThrowsExceptionWhenUserIsEntitledButBuildFailsTest() throws MachineNotCreatedException, UserNotEntitledException {
+        when(systemBuildService.createNewMachine(newMachine)).thenReturn("");
+        int initalFailedRequests = requestor.totalFailedBuildsForDay();
+
+        Assertions.assertThrows(MachineNotCreatedException.class,() -> requestor.createNewRequest(newMachine));
+
+        verify(systemBuildService,times(1)).createNewMachine(newMachine);
+    }
+
+
+//    @Test
+//    void requestorLogsFailedBuildWhenUserIsEntitledButBuildFailsTest() throws MachineNotCreatedException, UserNotEntitledException {
+//        when(systemBuildService.createNewMachine(newMachine)).thenReturn("");
+//        int initalFailedRequests = requestor.totalFailedBuildsForDay();
+//        requestor.createNewRequest(newMachine);
+//        int failedRequestsafterInvocation = requestor.totalFailedBuildsForDay();
+//
+//        Assertions.assertEquals(0, initalFailedRequests);
+//        Assertions.assertEquals(1, failedRequestsafterInvocation);
+//        verify(systemBuildService,times(1)).createNewMachine(newMachine);
+//    }
+
+
 
 
 }
